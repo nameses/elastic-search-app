ac4a03f96ea983be368404835860b8268aa8d608
Maxym Perebiynis <perebyinis.maksym@lll.kpi.ua>
2023-08-29 17:22:33 +0300

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace elastic_search_app.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger=logger;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetById(id);
+
+            if (user==null) return NotFound();
+
+            return Ok(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSerilogRequestLogging();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
