f4c6e8c6f8845249112d94f4bd12f57826662a36
Maxym Perebiynis <perebyinis.maksym@lll.kpi.ua>
2023-09-03 15:55:56 +0300

    added Hangfire logging and db support for hangfire (sqlexpress), added hidden settings json

---
+++ Settings/Configuration/ElasticConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace elastic_search_app.Settings.Configuration
+{
+    public class ElasticConfig
+    {
+        public string? CloudId { get; set; }
+        public string? ApiKey { get; set; }
+        public string? User { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
